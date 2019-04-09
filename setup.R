cc08bc269bbeea50c64e17814165d87268be448f
Miles McBain <miles.mcbain@gmail.com>
2019-04-09 15:51:14 +1000

    fix typo in setup.R

--- setup.R
+++ setup.R
@@ -2,5 +2,5 @@ packrat::restore()
 packrat::init(infer.dependencies = FALSE,
               options = list(
                 vcs.ignore.lib = TRUE,
-                vsc.ignore.src = TRUE
+                vcs.ignore.src = TRUE
               ))
